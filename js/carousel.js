9285f5212c2fd310907f762e8b52d2f7625e5110
Azriel <cauateixeirac2@gmail.com>
2021-06-25 02:40:17 -0300

    carousel funcionando

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,45 @@
-counter = 1
-const btn = document.querySelectorAll('.btn-carousel')
-var radio = document.getElementById('radio'+counter)
+var counter = 1
+const sldButtons = document.querySelectorAll('.btn-carousel')
 
-btn.forEach(button =>{
+ 
+
+
+sldButtons.forEach(button => {
     button.addEventListener('click', event => {
-        if(event.target.id === 'next'){
-            counter++;
-
-            if(counter >4){
-                counter = 1;
-            }
-        }else{
-            if(event.target.id === 'prev'){
+        if(event.target.id === 'prev'){
+            if(counter >= 1){
                 counter--;
+                console.log("prev "+counter);
+                document.getElementById('radio' + counter).checked = true;
+            }else{
                 if(counter < 1){
-                    counter = 4; 
+                    counter = 4;
+                    document.getElementById('radio' + counter).checked = true;
                 }
             }
+        }else{
+            if(event.target.id === 'next'){
+                if(counter <= 4){
+                    counter++;
+                    console.log("next "+counter);
+                    document.getElementById('radio' + counter).checked = true;
+                }else{
+                    if(counter > 4){
+                        counter = 1;
+                        document.getElementById('radio' + counter).checked = true;
+                    }
+                }
+            }
+            document.getElementById('radio' + counter).checked = true;
         }
-    })
+    });
 })
+setInterval( function(){
+    
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+    document.getElementById('radio' + counter).checked = true;
+    console.log("next "+counter);
+}, 3000) 
